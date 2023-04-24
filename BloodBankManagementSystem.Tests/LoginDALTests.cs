db53b8fa4dacd1b45af3910c8f91613e1734b794
Nidhan Bhatt <nbhatt@seattleu.edu>
2023-04-24 14:05:22 -0700

    Changes in LoginDal_Test made by nidhan.

--- BloodBankManagementSystem.Tests/LoginDALTests.cs
+++ BloodBankManagementSystem.Tests/LoginDALTests.cs
@@ -1,7 +1,13 @@
 using System.Collections.Generic;
+using System.Configuration;
+using System.Configuration.Internal;
 using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Reflection;
 using BloodBankManagementSystem.BLL;
 using BloodBankManagementSystem.DAL;
+using Castle.Core.Configuration;
 using Moq;
 using NUnit.Framework;
 
@@ -23,7 +29,7 @@ namespace BloodBankManagementSystem.Tests
             mockDbCommand = new Mock<IDbCommand>();
             mockParameter = new Mock<IDbDataParameter>();
             mockDataParameterCollection = new Mock<IDataParameterCollection>();
-            
+
             mockDbConnection.SetupAllProperties();
             mockDbDataAdapter.SetupAllProperties();
             mockDbCommand.SetupAllProperties();
@@ -32,10 +38,10 @@ namespace BloodBankManagementSystem.Tests
 
             mockDbCommand.Setup(m => m.CreateParameter()).Returns(mockParameter.Object);
             mockDbCommand.SetupGet(m => m.Parameters).Returns(mockDataParameterCollection.Object);
-            
+
             mockDbConnection.Setup(m => m.CreateCommand()).Returns(mockDbCommand.Object);
         }
-        
+
         [Test]
         public void loginDal_loginCheck_Success()
         {
@@ -58,12 +64,53 @@ namespace BloodBankManagementSystem.Tests
                 username = "admin",
                 password = "admin",
             };
-            
+
             var loginDal = new loginDAL(mockDbConnection.Object, mockDbDataAdapter.Object);
 
             var success = loginDal.loginCheck(loginBll);
-            
+
             Assert.True(success);
         }
+
+
+
+        [Test]
+        [TestCase("Nidhan", "123", ExpectedResult = false, TestName = "LoginDAL_check_false_Not_Available_inTable(Nidhan,123)")]
+        [TestCase("", "", ExpectedResult = false, TestName = "LoginDAL_check_false_Not_Available_inTable(,)")]
+        [TestCase(null, null, ExpectedResult = false, TestName = "LoginDAL_check_false_Not_Available_inTable(null,null)")]
+        [TestCase("Nidhan", "", ExpectedResult = false, TestName = "LoginDAL_check_false_Not_Available_inTable(Nidhan,)")]
+        [TestCase("", "123", ExpectedResult = false, TestName = "LoginDAL_check_false_Not_Available_inTable(,123)")]
+        [TestCase("Nidhan", null, ExpectedResult = false, TestName = "LoginDAL_check_false_Not_Available_inTable(Nidhan,null)")]
+        [TestCase(null, "123", ExpectedResult = false, TestName = "LoginDAL_check_false_Not_Available_inTable(null,123)")]
+        public Boolean loginDal_loginCheck_fail(String Login_Username, String Login_Password)
+        {
+            mockDbDataAdapter.Setup(m => m.Fill(It.IsAny<DataSet>())).Returns((DataSet dataSet) =>
+            {
+                // Create mock data
+                var table = new DataTable("MockUserTable");
+                table.Columns.Add("username", typeof(string));
+                table.Columns.Add("password", typeof(string));
+                // Add the mock data to the dataset
+                dataSet.Tables.Add(table);
+
+
+                return table.Rows.Count;
+            });
+
+            var loginBll = new loginBLL
+            {
+                username = Login_Username,
+                password = Login_Password,
+            };
+
+            var loginDal = new loginDAL(mockDbConnection.Object, mockDbDataAdapter.Object);
+
+            var failure = loginDal.loginCheck(loginBll);
+
+            return failure;
+
+            /*Assert.True(success);*/
+        }
+
     }
 }
