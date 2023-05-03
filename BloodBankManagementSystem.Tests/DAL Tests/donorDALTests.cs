cb44f36d973cf9ef7ac5a963c9e524cc295124d6
Nidhan Bhatt <nbhatt@seattleu.edu>
2023-05-02 20:18:38 -0700

    Donor dal test

---
+++ BloodBankManagementSystem.Tests/DAL Tests/donorDALTests.cs
@@ -0,0 +1,46 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBankManagementSystem.Tests.DAL_Tests
+{
+    public class donorDALTests
+    {
+
+        private Mock<IDbConnection> mockDbConnection;
+        private Mock<IDbDataAdapter> mockDbDataAdapter;
+        private Mock<IDbCommand> mockDbCommand;
+        private Mock<IDbDataParameter> mockParameter;
+        private Mock<IDataParameterCollection> mockDataParameterCollection;
+
+        [SetUp]
+        public void Setup()
+        {
+            mockDbConnection = new Mock<IDbConnection>();
+            mockDbDataAdapter = new Mock<IDbDataAdapter>();
+            mockDbCommand = new Mock<IDbCommand>();
+            mockParameter = new Mock<IDbDataParameter>();
+            mockDataParameterCollection = new Mock<IDataParameterCollection>();
+
+            mockDbConnection.SetupAllProperties();
+            mockDbDataAdapter.SetupAllProperties();
+            mockDbCommand.SetupAllProperties();
+            mockParameter.SetupAllProperties();
+            mockDataParameterCollection.SetupAllProperties();
+
+            mockDbCommand.Setup(m => m.CreateParameter()).Returns(mockParameter.Object);
+            mockDbCommand.SetupGet(m => m.Parameters).Returns(mockDataParameterCollection.Object);
+
+            mockDbConnection.Setup(m => m.CreateCommand()).Returns(mockDbCommand.Object);
+        }
+
+
+
+
+    }
+}
