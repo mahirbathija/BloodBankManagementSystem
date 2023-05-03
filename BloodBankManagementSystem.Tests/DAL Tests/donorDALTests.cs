9f495d29b7b8c5b7333e7b2b4172c9cddc177980
Nidhan Bhatt <nbhatt@seattleu.edu>
2023-05-02 21:36:54 -0700

    Commited donor dal test

--- BloodBankManagementSystem.Tests/DAL Tests/donorDALTests.cs
+++ BloodBankManagementSystem.Tests/DAL Tests/donorDALTests.cs
@@ -1,4 +1,6 @@
-﻿using Moq;
+﻿using BloodBankManagementSystem.BLL;
+using BloodBankManagementSystem.DAL;
+using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -39,8 +41,62 @@ namespace BloodBankManagementSystem.Tests.DAL_Tests
             mockDbConnection.Setup(m => m.CreateCommand()).Returns(mockDbCommand.Object);
         }
 
+        [Test]
+        [TestCase(1, ExpectedResult = true, TestName = "DonorDAL_check_true_Available_inTable(1)")]
+        public Boolean DonorDALDelete_Success(int Donor_id)
+        {
+        
+            // Setup the mock IDbConnection to return the mock IDbCommand
+            mockDbConnection.Setup(x => x.CreateCommand()).Returns(mockDbCommand.Object);
+
+            // Setup the mock IDbCommand to return the mock IDataParameterCollection
+            mockDbCommand.Setup(x => x.Parameters).Returns(mockDataParameterCollection.Object);
+
+            // Setup the mock IDbCommand to return a value of 1 when ExecuteNonQuery is called
+            mockDbCommand.Setup(x => x.ExecuteNonQuery()).Returns(1);
+
+            var donor = new donorBLL { donor_id = Donor_id };
+
+            var dal = new donorDAL(mockDbConnection.Object,mockDbDataAdapter.Object);
+
+            // Act
+            var result = dal.Delete(donor);
+
+            // Assert
+            return result;
+        }
+
+
+        [Test]
+        [TestCase(2, ExpectedResult = false, TestName = "DonorDAL_check_false_NotAvailable_inTable(2)")]
+        public Boolean DonorDALDelete_Failure(int Donor_id)
+        {
+
+            // Setup the mock IDbConnection to return the mock IDbCommand
+            mockDbConnection.Setup(x => x.CreateCommand()).Returns(mockDbCommand.Object);
+
+            // Setup the mock IDbCommand to return the mock IDataParameterCollection
+            mockDbCommand.Setup(x => x.Parameters).Returns(mockDataParameterCollection.Object);
+
+            // Setup the mock IDbCommand to return a value of 1 when ExecuteNonQuery is called
+            mockDbCommand.Setup(x => x.ExecuteNonQuery()).Returns(0);
+
+            var donor = new donorBLL { donor_id = Donor_id };
+
+            var dal = new donorDAL(mockDbConnection.Object, mockDbDataAdapter.Object);
+
+            // Act
+            var result = dal.Delete(donor);
+
+            // Assert
+            return result;
+        }
+
+
 
 
 
     }
 }
+
+
